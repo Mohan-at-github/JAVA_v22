800ef365e1d3803aa1a4b9c77b1079dfaf5b8007
Mohan-at-github <mohanrr17@gmail.com>
2025-03-15 14:05:28 +0530

    ArrayList and compartor

--- src/Collections/Comparator_Comapre_Demo.java
+++ src/Collections/Comparator_Comapre_Demo.java
@@ -1,2 +1,144 @@
-package Collections;public class Comparator_Comapre_Demo {
+package Collections;
+
+import Strings.A;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+class MyCompartor implements Comparator<Integer> {
+
+    @Override
+    public int compare(Integer o1, Integer o2) {
+        return o1 - o2;
+    }
 }
+
+ class MyCompartor1 implements Comparator<Integer> {
+
+        @Override
+        public int compare(Integer o1, Integer o2) {
+            return o2 - o1;
+        }
+    }
+
+    class StringCompartor1 implements Comparator<String>{
+
+        @Override
+        public int compare(String o1, String o2) {
+           return o1.length()-o2.length();
+        }
+    }
+
+class StringCompartor2 implements Comparator<String>{
+
+    @Override
+    public int compare(String o1, String o2) {
+        return o2.length()-o1.length();
+    }
+}
+    class Student{
+        String name;
+        double cgp;
+
+        public Student(String str, double v) {
+            this.name=str;
+            this.cgp=v;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public double getCgp() {
+            return cgp;
+        }
+
+        public void setCgp(double cgp) {
+            this.cgp = cgp;
+        }
+    }
+
+    public  class Comparator_Comapre_Demo {
+        public void main(String[] args) {
+            //Example:1
+            List<Integer> list1=new ArrayList<>();
+            list1.add(1);
+            list1.add(8);
+            list1.add(3);
+            list1.add(90);
+            System.out.println(list1);
+            list1.sort(null); //default natural order (asc order)
+            System.out.println(list1);
+
+            list1.sort(new MyCompartor()); //default natural order (asc order) using comparator
+            System.out.println(list1);
+
+            list1.sort(new MyCompartor1()); //default natural order (asc order) using comparator
+            System.out.println(list1);
+
+            //Example:2
+            List<String> list2=new ArrayList<>();
+            list2.add("Banana");
+            list2.add("Apple");
+            list2.add("PineApple");
+            list2.sort(new StringCompartor1()); //asc order
+            System.out.println(list2);
+
+            list2.sort(new StringCompartor2()); //desc order
+            System.out.println(list2);
+
+            //using Lambda exp
+            list2.sort((a,b)->a.length()-b.length());
+            System.out.println(list2);
+            list2.sort((a,b)->b.length()-a.length());
+            System.out.println(list2);
+
+            //Example:3
+            List<Student> studentList=new ArrayList<>();
+            studentList.add(new Student("raj",9.8));
+            studentList.add(new Student("mohan",9.8));
+            studentList.add(new Student("Suhas",6.8));
+            studentList.add(new Student("Lohith",8.8));
+            for(Student std:studentList){
+                System.out.println(std.getName()+" "+std.getCgp());
+            }
+            System.out.println();
+           // studentList.sort((a,b)-> (int) (b.getCgp()-a.getCgp())); //natural order but it's not possible
+            // because it doesn't know on what bases we need to sort,
+            // so we need to use comparable interface
+            studentList.sort((a,b)->{
+                    if((b.getCgp()-a.getCgp())>1){
+                    return 1;
+                }
+               else if((b.getCgp()-a.getCgp())<1){
+                    return -1;
+                }
+               else {
+                   return 0;
+                    }
+                });
+            for(Student std:studentList){
+                System.out.println(std.getName()+" "+std.getCgp());
+            }
+
+            System.out.println();
+            Comparator<Student> studentComparator = Comparator.comparing(Student::getCgp);
+            studentList.sort(studentComparator);
+            for(Student std:studentList){
+                System.out.println(std.getName()+" "+std.getCgp());
+            }
+
+            System.out.println();
+            Comparator<Student> studentComparator1 = Comparator.comparing(Student::getCgp).reversed().thenComparing(Student::getName);
+            studentList.sort(studentComparator1);
+            for(Student std:studentList){
+                System.out.println(std.getName()+" "+std.getCgp());
+            }
+        }
+    }
+
--- src/Collections/arrayList.java
+++ src/Collections/arrayList.java
@@ -1,2 +1,64 @@
-package Collections;public class ArrayList {
+package Collections;
+
+import java.util.*;
+
+public class arrayList {
+    public static void main(String[] args) {
+        //adding elements to ArrayList
+       List<Integer> list=new ArrayList<>();
+       list.add(1);
+       list.add(3);
+       list.add(80);
+       list.add(99);
+
+        //adding one ArrayList to another ArrayList
+       List<Integer> list1=Arrays.asList(6,7,4,3); // only updating existing element is possible
+       List<Integer> list2=List.of(10,20,30,40); // modification is not possible
+       list.addAll(list1);
+        System.out.println(list);
+
+        //to update a element for a particular Index
+        list.set(2, 2003);
+        System.out.println(list);
+
+        //Printing ArrayList
+       System.out.println(list.get(1));
+       list.add(1,20);
+
+        System.out.println(list.get(1));
+        System.out.println(list.get(4));
+        System.out.println("");
+        for(int i=0;i<list.size();i++){
+            System.out.println(list.get(i));
+        }
+
+        for (Integer integer : list) {
+            System.out.println(integer);
+        }
+
+        Iterator<Integer> it=list.iterator();
+        while(it.hasNext()){
+            System.out.println("Iterator: "+it.next());
+        }
+
+        //Removing element from Arraylist
+        System.out.println(list);
+        list.remove(2);
+        System.out.println(list);
+        list.remove(Integer.valueOf(20));
+        System.out.println(list);
+        list.remove(4); //removing element using Index
+        System.out.println(list);
+
+        //Checking element present in ArrayList
+        System.out.println(list.contains(80));
+        System.out.println(list.contains(290));
+
+        //clearing entire ArrayList
+        list.clear();
+        System.out.println(list);
+
+        List<Integer> List4=new ArrayList<>(1000);
+
+    }
 }
